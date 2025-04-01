494ad00be307afca55299b587b95f9160f908ed0
Shekhu04 <gshikhar04@gmail.com>
2025-04-01 10:37:57 +0530

    profile page created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,30 @@
 import express from "express";
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin : "http://localhost:5173",
-  credentials : true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth" , authRoutes)
-app.use("/api/message" , messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT : " + PORT);
-  connectDB()
+  connectDB();
 });
